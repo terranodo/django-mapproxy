bfaf983e21aeb47f442e315a12ce67c833a692b0
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-06-21 10:46:59 +0200

    add admin and make urls django 1.8 compliant

--- djmp/urls.py
+++ djmp/urls.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.conf.urls import include, patterns, url
+from django.contrib import admin
 
 from .views import DetailView, seed, tileset_status
 
-urlpatterns = [
+admin.autodiscover()
+
+urlpatterns = patterns('',
 
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='tileset_detail'),
     url(r'^(?P<pk>\d+)/seed$', seed, name='tileset_seed'),
     url(r'^(?P<pk>\d+)/status$', tileset_status, name='tileset_status'),
-]
+    (r'^admin/', include(admin.site.urls)),
+)
