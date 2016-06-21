b5fdff8f9be1d356b5bdb765688af3f09626209e
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-06-21 09:57:00 +0200

    add seed test

--- djmp/tests.py
+++ djmp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from .views import tileset_status, seed
+from .models import Tileset
+
+
+class DjmpTest(TestCase):
+
+    fixtures = ['test_data.json']
+
+    def setUp(self):
+        self.user = 'admin'
+        self.passwd = 'admin'
+        self.client = Client()
+
+    def test_seeding(self):
+        """ test seeding"""
+        self.client.login(username='admin', password='admin')
+        resp = self.client.get(reverse('tileset_seed', args=(1,)))
+        self.assertEqual('{"status": "started"}', resp.content)
