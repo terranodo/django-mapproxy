be39f4ffd95da8666b8afa81b1b63cbaadf3f457
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-07-12 10:01:35 +0200

    reorganize config

--- djmp/mapproxy_config.py
+++ djmp/mapproxy_config.py
@@ -2,6 +2,81 @@ import json
 import os
 import base64
 
+def wms_source(tileset):
+    http = {}
+    if tileset.server_username and tileset.server_password:
+        encoded = base64.b64encode('{}:{}'.format(tileset.server_username, tileset.server_password))
+        http = {
+            "headers":{
+                "Authorization": 'Basic {}'.format(encoded)
+            },
+            "ssl_no_cert_checks": True
+        }
+
+    return {
+        "type": "wms",
+        "req": {
+            "url": u_to_str(tileset.server_url),
+            "layers": u_to_str(tileset.layer_name),
+            "transparent": True
+        },
+        "http": http
+    }
+
+def mapnik_source(tileset):
+    return {
+        "type": "mapnik",
+        "mapfile": tileset.mapfile.path,
+        "layers": [u_to_str(tileset.name)],
+        "transparent": True,
+        "coverage": {
+            "bbox": tileset.bbox_3857(),
+            "srs": 'EPSG:3857'
+        }
+    }
+
+def tile_source(tileset):
+    return {
+        "type": "tile",
+        "url": u_to_str(tileset.server_url)
+    }
+
+def file_cache(tileset):
+    return {
+        "type": "file",
+        "directory": os.path.join(tileset.directory, tileset.name),
+        "directory_layout": tileset.directory_layout
+    }
+
+def gpkg_cache(tileset):
+    return {
+        "type": "gpkg",
+        "filename": tileset.filename,
+        "table_name": tileset.table_name
+    }
+
+def seed_coverage(tileset):
+    return {
+        "bbox": tileset.bbox_3857(),
+        "srs": "EPSG:3857"
+    }
+
+def seed_seeds(tileset):
+    if tileset.layer_zoom_start > tileset.layer_zoom_stop:
+        raise ConfigurationError('invalid configuration - zoom start is greater than zoom stop')
+    return {
+        "refresh_before": {
+            "minutes": 0
+        },
+        "caches": [
+            "tileset_cache"
+        ],
+        "levels": {
+            "from": tileset.layer_zoom_start,
+            "to": tileset.layer_zoom_stop
+        },
+        "coverages": ["tileset_geom"]
+    }
 
 services_conf = {
     'wms': {'image_formats': ['image/png'],
@@ -20,32 +95,6 @@ services_conf = {
     'demo': None
 }
 
-layers_conf = [{
-    "name": "",
-    "title": "",
-    "sources":[
-        "tileset_cache"
-    ]
-}]
-
-caches_conf = {
-    "tileset_cache":{
-        "grids":[
-            "EPSG3857"
-        ],
-        "sources":[
-            "tileset_source"
-        ],
-        "cache": {}
-    }
-}
-
-sources_conf = {
-    "tileset_source":{
-        "type": ""
-    }
-}
-
 grids_conf = {
     "EPSG3857": {
         "origin": "nw",
@@ -53,107 +102,63 @@ grids_conf = {
     }
 }
 
-
-seed_coverages_conf = {
-    "tileset_geom": {
-        "bbox": [],
-        "srs": "EPSG:3857"
-    }
+sources_conf = {
+    "wms": wms_source,
+    "mapnik": mapnik_source,
+    "tile": tile_source
 }
 
-seed_seeds_conf = {
-    "tileset_seed": {
-        "refresh_before": {
-            "minutes": 0
-        },
-        "caches": [
-            "tileset_cache"
-        ],
-        "levels": {
-            "from": 0,
-            "to": 12
-        },
-        "coverages": ["tileset_geom"]
-    }
+cache_conf = {
+    "file": file_cache,
+    "gpkg": gpkg_cache
 }
 
 
 def get_mapproxy_conf(tileset):
-
-    # cache
-    caches_conf['tileset_cache']['cache']['type'] = tileset.cache_type
-
-    if tileset.cache_type == 'file':
-        caches_conf['tileset_cache']['cache']['directory'] = os.path.join(tileset.directory, tileset.name)
-        caches_conf['tileset_cache']['cache']['directory_layout'] = tileset.directory_layout
-    if tileset.cache_type == 'gpkg':
-        caches_conf['tileset_cache']['cache']['filename'] = tileset.filename
-        caches_conf['tileset_cache']['cache']['table_name'] = tileset.table_name
-
-    # source
-    source_type = tileset.source_type
-
-    sources_conf['tileset_source']['type'] = u_to_str(source_type)
-
-    if source_type == 'mapnik':
-        sources_conf['tileset_source']['mapfile'] = tileset.mapfile.path 
-        sources_conf['tileset_source']['layers'] = [u_to_str(tileset.name)]
-        sources_conf['tileset_source']['transparent'] = True
-        sources_conf['tileset_source']['coverage'] = {}
-        sources_conf['tileset_source']['coverage']['bbox'] = tileset.bbox_3857()
-        sources_conf['tileset_source']['coverage']['srs'] = 'EPSG:3857'
-
-    elif source_type == 'wms':
-        sources_conf['tileset_source']['req'] = {}
-        sources_conf['tileset_source']['req']['url'] = u_to_str(tileset.server_url)
-        sources_conf['tileset_source']['req']['layers'] = u_to_str(tileset.layer_name)
-        sources_conf['tileset_source']['req']['transparent'] = 'true'
-
-    elif source_type == 'tile':
-        sources_conf['sources']['tileset_source']['url'] = u_to_str(tileset.server_url)
-
-    if tileset.server_username and tileset.server_password:
-        encoded = base64.b64encode('{}:{}'.format(tileset.server_username, tileset.server_password))
-        sources_conf['tileset_source']['http'] = {}
-        sources_conf['tileset_source']['http']['headers'] = {}
-        sources_conf['tileset_source']['http']['headers']['Authorization'] = 'Basic {}'.format(encoded)
-        sources_conf['tileset_source']['http']['ssl_no_cert_checks'] = True
-
-    # layers
-    layers_conf[0]['name'] = u_to_str(tileset.name)
-    layers_conf[0]['title'] = u_to_str(tileset.name)
-
     return json.dumps({
-            'services': services_conf,
-            'layers': layers_conf,
-            'caches': caches_conf,
-            'sources': sources_conf,
-            'grids': grids_conf,
-            'globals': {
-                "image": {
-                    "paletted": False
-                },
-                'http': {'ssl_no_cert_checks': True},
+        'services': services_conf,
+        'layers':  [{
+            "name": u_to_str(tileset.name),
+            "title": u_to_str(tileset.name),
+            "sources":[
+                "tileset_cache"
+            ]
+        }],
+        'caches': {
+            "tileset_cache":{
+                "grids":[
+                    "EPSG3857"
+                ],
+                "sources":[
+                    "tileset_source"
+                ],
+                "cache": cache_conf.get(tileset.cache_type)(tileset)
             }
-        })
+        },
+        'sources': {
+            'tileset_source': sources_conf.get(tileset.source_type)(tileset)
+        },  
+        'grids': grids_conf,
+        'globals': {
+            "image": {
+                "paletted": False
+            },
+            'http': {'ssl_no_cert_checks': True},
+        }
+    })
 
 def get_seed_conf(tileset):
 
     seed_conf = {
-        'coverages': seed_coverages_conf,
-        'seeds': seed_seeds_conf
+        'coverages': {
+            "tileset_geom": seed_coverage(tileset)
+        },
+        'seeds': {
+            "tileset_seed": seed_seeds(tileset)
+        }
     }
-
-    if tileset.layer_zoom_start > tileset.layer_zoom_stop:
-        raise ConfigurationError('invalid configuration - zoom start is greater than zoom stop')
-    seed_conf['seeds']['tileset_seed']['levels']['from'] = tileset.layer_zoom_start
-    seed_conf['seeds']['tileset_seed']['levels']['to'] = tileset.layer_zoom_stop
-
-    seed_conf['coverages']['tileset_geom']['bbox'] = tileset.bbox_3857()
     
     return json.dumps(seed_conf)
-
-
 
 def u_to_str(string):
     return string.encode('ascii', 'ignore')
