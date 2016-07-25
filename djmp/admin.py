0ef1524a5e586f7a555ea2b55b05ee10a7d35686
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-07-25 08:43:13 +0200

    add seeding custom action

--- djmp/admin.py
+++ djmp/admin.py
@@ -3,9 +3,18 @@ from guardian.admin import GuardedModelAdmin
 
 from .models import Tileset
 
+
+def seed_action(modeladmin, request, queryset):
+    for tileset in queryset:
+        tileset.seed()
+
+seed_action.short_description = "Seed selected Tilesets"
+
+
 class TilesetAdmin(GuardedModelAdmin):
     readonly_fields = ('size', 'layer_uuid',)
     list_display = ('name', 'layer_name', 'server_url', 'created_by', 'created_at')
     search_fields = ['name']
+    actions = [seed_action]
 
 admin.site.register(Tileset, TilesetAdmin)
