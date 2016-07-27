08fe84036dd628a399de3053ea23db44836efbf0
Michael Van Veen <michael@mvanveen.net>
2016-07-27 12:00:50 -0700

    use django.conf.settings instead of .settings

--- djmp/api.py
+++ djmp/api.py
@@ -1,13 +1,12 @@
 import importlib
 
+from django.conf import settings
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.resources import ModelResource
 
 from .models import Tileset
 
-from .settings import DJMP_AUTHORIZATION_CLASS
-
-module_name, class_name = DJMP_AUTHORIZATION_CLASS.rsplit(".", 1)
+module_name, class_name = settings.DJMP_AUTHORIZATION_CLASS.rsplit(".", 1)
 auth_class = getattr(importlib.import_module(module_name), class_name)
 
 
